c3e0ee9e0a91aa2b3cf6e1c29b111fd50957125e
johndornstadteriii <johndornstadteriii@github.com>
2026-04-19 21:00:24 -0500

    Remove GitHub Pages config, switching to Vercel

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  trailingSlash: true,
-  basePath: "/fodbot-website",
-  assetPrefix: "/fodbot-website",
   images: {
-    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "img.youtube.com" },
       { protocol: "https", hostname: "i.ytimg.com" },
